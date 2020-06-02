6b98db11cdffa2276624070a84f7eb718842eff1
Kabir Maan <kabirmaan9@gmail.com>
2020-06-02 17:10:21 +0100

    Setup test sendMail funciton

--- notification-service/src/handlers/sendMail.js
+++ notification-service/src/handlers/sendMail.js
@@ -1,6 +1,32 @@
+import AWS from "aws-sdk";
+
+const ses = new AWS.SES({ region: "eu-west-1" });
+
 async function sendMail(event, context) {
-  console.log(event);
-  return event;
+  const params = {
+    Source: "kabirmaan9@gmail.com",
+    Destination: {
+      ToAddresses: ["kabirmaan11@gmail.com"],
+    },
+    Message: {
+      Body: {
+        Text: {
+          Data: "Hello from Kabir",
+        },
+      },
+      Subject: {
+        Data: "Test Mail",
+      },
+    },
+  };
+
+  try {
+    const result = await ses.sendEmail(params).promise();
+    console.log(result);
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 export const handler = sendMail;
