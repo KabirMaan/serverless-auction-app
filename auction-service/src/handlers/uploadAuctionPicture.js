39d9b6defcf3a57bb8393e88d6b92047daab0c9d
Kabir Maan <kabirmaan9@gmail.com>
2020-06-03 01:43:16 +0100

    Set picture URL in DynamoDB

--- auction-service/src/handlers/uploadAuctionPicture.js
+++ auction-service/src/handlers/uploadAuctionPicture.js
@@ -3,19 +3,25 @@ import httpErrorHandler from "@middy/http-error-handler";
 import createError from "http-errors";
 import { getAuctionById } from "./getAuction";
 import { uploadPictureToS3 } from "../lib/uploadPictureToS3";
+import { setAuctionPictureUrl } from "../lib/setAuctionPictureUrl";
 
 export async function uploadAuctionPicture(event) {
   const { id } = event.pathParameters;
+  const { email } = event.requestContext.authorizer;
   const auction = await getAuctionById(id);
   const base64 = event.body.replace(/^data:image\/\w+;base64,/, "");
   const buffer = Buffer.from(base64, "base64");
 
+  let updatedAuction;
+
+  // Auction identity
+  if (email !== auction.seller) {
+    throw new createError.Forbidden("You are not the seller of this auction!");
+  }
+
   try {
-    const uploadToS3Result = await uploadPictureToS3(
-      auction.id + ".jpg",
-      buffer
-    );
-    console.log(uploadToS3Result);
+    const pictureUrl = await uploadPictureToS3(auction.id + ".jpg", buffer);
+    updatedAuction = await setAuctionPictureUrl(auction.id, pictureUrl);
   } catch (error) {
     console.log(error);
     throw new createError.InternalServerError(error);
@@ -23,8 +29,8 @@ export async function uploadAuctionPicture(event) {
 
   return {
     statusCode: 200,
-    body: JSON.stringify({}),
+    body: JSON.stringify(updatedAuction),
   };
 }
 
-export const handler = middy(uploadAuctionPicture).use(httpErrorHandler);
+export const handler = middy(uploadAuctionPicture).use(httpErrorHandler());
